2422469f01cbc3db8c0cace27ff7deff22791643
Hugo Rydback <hugo.rydback@skola.taby.se>
2026-04-02 12:34:33 +0200

    Har kopplat java script, sedan lagt lite enkel javascript kod, för nu kan man se när man söker i console nu sedan är tanken att fixa en gratis api

---
+++ slutprojekt.js
@@ -0,0 +1,6 @@
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+
+searchBtn.addEventListener("click", function() {
+    console.log(searchInput.value);
+});
